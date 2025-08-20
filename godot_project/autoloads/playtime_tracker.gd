3aaf72d20c5c570e259d7d1fd9f2e4614671192d
jupiterbjy <jupiterbjy@gmail.com>
2025-08-20 10:01:28 +0900

    Typo fix on PlaytimeTracker's query

--- godot_project/autoloads/playtime_tracker.gd
+++ godot_project/autoloads/playtime_tracker.gd
@@ -23,20 +23,20 @@ class _Query:
 	# Should I cascade? idk
 
 	const create_table := """
-	CREATE TABLE IF NOT EXISTS "{}" (start_utc INTEGER, end_utc INTEGER, time REAL, PRIMARY KEY(start_utc))
+	CREATE TABLE IF NOT EXISTS "%s" (start_utc INTEGER, end_utc INTEGER, time REAL, PRIMARY KEY(start_utc))
 	"""
 
-	const drop_table := 'DROP TABLE IF EXISTS "{}"'
+	const drop_table := 'DROP TABLE IF EXISTS "%s"'
 
 	const add_or_update_time := """
-	INSERT INTO "{}" VALUES (?, ?, ?) ON CONFLICT(start_utc) DO UPDATE SET end_utc=?, time=time+?
+	INSERT INTO "%s" VALUES (?, ?, ?) ON CONFLICT(start_utc) DO UPDATE SET end_utc=?, time=time+?
 	"""
 
-	const get_total_time := 'SELECT SUM(time) FROM "{}"'
+	const get_total_time := 'SELECT SUM(time) FROM "%s"'
 
-	const get_single_time := 'SELECT time FROM "{}" WHERE start_utc=?'
+	const get_single_time := 'SELECT time FROM "%s" WHERE start_utc=?'
 
-	const get_all_sessions := 'SELECT * FROM "{}"'
+	const get_all_sessions := 'SELECT * FROM "%s"'
 
 	# TODO: get last playtime
 
@@ -160,4 +160,4 @@ func _on_entry_added(entry: EntryManager.Entry) -> void:
 func _on_entry_removed(entry: EntryManager.Entry) -> void:
 	self._path_to_db_id[entry.exec_path].erase(entry.db_id)
 
-	self._db.execute(_Query.drop_table, [entry.db_id])
+	self._db.execute(_Query.drop_table % entry.db_id)
